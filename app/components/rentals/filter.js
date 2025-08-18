66b46dc2fe491abf8226c5aff5978ac49227e5b7
AathiEswar <aathi28082003@gmail.com>
2025-08-18 10:16:33 +0530

    provider component

---
+++ app/components/rentals/filter.js
@@ -0,0 +1,13 @@
+import Component from '@glimmer/component';
+
+export default class RentalsFilter extends Component {
+  get results() {
+    let { rentals, query } = this.args;
+
+    if (query) {
+      rentals = rentals.filter((rental) => rental.title.toLowerCase().includes(query));
+    }
+
+    return rentals;
+  }
+}
